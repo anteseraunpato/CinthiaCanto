a3058da6065d46c50f7ac9054fb4ccfb997ea413
Jahir Medina <jahirtunmedina@gmail.com>
2024-09-18 15:48:06 -0600

    Add animations to navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 z-0">
+{/*Image Container */}
+<div className="h-1/2 lg:h-full lg:w-1/2 relative">
+  <Image src="/hero.jpg" alt="" fill className="object-contain"/>
+</div>
+
+
+{/*Text Container */}
+
+<div className="h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 items-center justify-center z-10">
+{/*Título */}
+<h1 className="text-4xl md:text-6xl font-bold">Tu bienestar, tu voz, tu camino hacia la libertad</h1>
+{/*Descripción*/}
+<p className="md:text-xl">Soy Cinthia Canto, me dedico a brindar un espacio seguro e inclusivo para mujeres y personas LGBT+. Con perspectiva de género y centrada en el bienestar emocional, te acompaño en tu proceso de crecimiento personal, autodescubrimiento y sanación, ofreciéndote apoyo cercano y empático en cada paso de tu camino.</p>
+
+
+
+
+
+<div className="w-full flex gap-4">
+  {/*Botones */}
+  <button className="p-4 rounded-lg ring-1 ring-[#8b7ab7] bg-[#8b7ab7] text-white">Platiquemos</button>
+  <button className="p-4 rounded-lg ring-1 ring-purple-500">Agendar</button>
+  </div>
+</div>
+  </div>;
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import Link from "next/link"
 import { useState } from "react";
 import NavLink from "./navLink";
+import {motion} from "framer-motion";
 
 
 const links = [
     {url: "/", title:"Home"},
-    {url: "/about", title:"Sobre mi"},
+    {url: "/about", title:"Acerca"},
     {url: "/portfolio", title:"Portafolio"},
     {url: "/contact", title:"Contacto"},
     ];
@@ -15,6 +16,65 @@ const links = [
 const Navbar = () => {
 
     const [open, setOpen] = useState (false)
+
+    const topVariants={
+        closed:{
+        rotate: 0,
+        },
+
+        opened: {
+            rotate: 45,
+            backgroundColor:"rgb(255,255,255)",
+        },
+    }
+
+    const centerVariants={
+        closed:{
+        opacity: 1,
+        },
+
+        opened: {
+            opacity: 0,
+        },
+    }
+
+    const bottomVariants={
+        closed:{
+        rotate: 0,
+        },
+
+        opened: {
+            rotate: -45,
+            backgroundColor:"rgb(255,255,255)",
+        },
+    }
+
+    const listVariants = {
+        closed: {
+            x:"100vw",
+        },
+
+        opened: {
+            x: 0,
+            transition:{
+                staggerChildren: 0.2,
+            },
+        },
+    }
+
+    const listItemVariants = {
+        closed: {
+            x: -100,
+            opacity: 0,
+        },
+
+        opened: {
+            x: 0,
+            opacity: 1,
+        },
+    }
+
+
     
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
@@ -25,10 +85,9 @@ const Navbar = () => {
                 ))}
             </div>
             {/* Logo */}
-            <div className="md:hidden lg:flex w-1/3  justify-center">
-                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-                <span className="text-white mr-1">Cinthia</span>
-                <span className="w-12 h-8 bg-white text-black flex items-center justify-center">Canto</span>
+            <div className=" lg:flex w-1/3  justify-center">
+                <Link href="/" className="text-sm  rounded-md p-1 font-semibold flex items-center justify-center sm:justify-start sm:hidden">
+                <Image src="/logo.jpg" alt="" width={32} height={32} className="object-contain"/>
                 </Link>
             </div>
             {/*Redes sociales*/}
@@ -47,19 +106,22 @@ const Navbar = () => {
             {/* Mobile */}
             <div className="md:hidden">
                 <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(()=>setOpen(!open))}>
-                    <div className="w-10 h-1 bg-slate-400 rounded"></div>
-                    <div className="w-10 h-1 bg-slate-400 rounded"></div>
-                    <div className="w-10 h-1 bg-slate-400 rounded"></div>
+                    <motion.div variants={topVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-slate-400 rounded origin-left"></motion.div>
+                    <motion.div variants={centerVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-slate-400 rounded"></motion.div>
+                    <motion.div variants={bottomVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-slate-400 rounded origin-left"></motion.div>
                 </button>
 
                 {/* Items del menu*/}
 
                 {open &&(
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-2xl">
+                <motion.div variants={listVariants} initial="closed" animate="opened" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-2xl z-40">
+                    
                     {links.map(link => (
-                        <Link href={link.url} key={link.title}>{link.title}</Link>
+                        <motion.div variants={listItemVariants} key={link.title}>
+                        <Link href={link.url}>{link.title}</Link>
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
             )}
             </div>
         </div>
